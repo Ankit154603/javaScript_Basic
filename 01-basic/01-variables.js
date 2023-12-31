f81b5fe75d049c9f12cce5e12069a3bba6b79112
Ankit154603 <ankit33242@gmail.com>
2023-12-31 18:34:25 +0545

    Add Initial

---
+++ 01-basic/01-variables.js
@@ -0,0 +1,15 @@
+// Variables
+
+const employeeId = 12456;
+
+let employeeName = "jaii";
+
+var employeeAge = 23;
+
+employeeCity = "Kathmandu";
+
+// Changing variable value
+employeeName = "Rahul";
+employeeAge = 24;
+employeeCity = "Ayodhya";
+console.table([employeeId,employeeName,employeeAge,employeeCity]);
