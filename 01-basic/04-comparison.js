3f09149f70411bc56cc4451114266c263912b29d
Ankit154603 <ankit33242@gmail.com>
2024-01-01 09:20:26 +0545

    Update File

--- 01-basic/04-comparison.js
+++ 01-basic/04-comparison.js
@@ -1,6 +1,19 @@
-console.log(2 > 3);
-console.log(2 >= 3);
-console.log(2 < 3);
-console.log(2 <= 3);
-console.log(2 == 3);
-console.log(2 != 3);
+// console.log(2 > 3);
+// console.log(2 >= 3);
+// console.log(2 < 3);
+// console.log(2 <= 3);
+// console.log(2 == 3);
+// console.log(2 != 3);
+
+// console.log("2" > 1);
+// console.log("2" < 1);
+
+// console.log(null == 0);
+// console.log(null > 0);
+// console.log(null >= 0);
+// console.log(null < 0);
+
+// === 
+
+console.log("2" == 2); // == checks only number criteria
+console.log("2" === 2)  // === checks datatype also
