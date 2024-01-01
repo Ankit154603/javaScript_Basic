9a3a7d38109d75ccab58b6496955688c8ef1c669
Ankit154603 <ankit33242@gmail.com>
2024-01-01 08:35:51 +0545

    Add Files

---
+++ 01-basic/02-dataTypes.js
@@ -0,0 +1,11 @@
+// number => countable 
+// bigint => bigint use for large number
+// boolean => true/flase
+// string = " "
+// null => stand alone number
+// undefined => 
+// symbol => refers to his uniqueness
+// objects
+
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 01-basic/03-conversationOperation.js
@@ -0,0 +1,61 @@
+let score = "true";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => number
+// "33abc" => NaN
+// "ram" => NaN
+
+let isLoggedIn = "0";
+
+let booleanIsLogged = Boolean(isLoggedIn);
+
+// console.log(booleanIsLogged);
+
+
+
+// ****************  Operation  ************************
+
+let value = 3;
+
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log("2"+3);
+// console.log(2+"3");
+// console.log(2+3+"4");
+
+let str1 = "Jai";
+let str2 = " Shree";
+let str3 = " Ram";
+
+let result = str1 + str2 + str3;
+
+// console.log(result);
+
+// console.log(10+2); // Addition
+// console.log(10-2); // Substraction
+// console.log(10*2); // Multiplication
+// console.log(10**3); // Power
+// console.log(10/2); // Division  // It's gives quotients
+// console.log(10%2); // Modulus  // it's gives remainder 
+
+// console.log(true);
+// console.log(+true);
+// console.log(true+);
+
+let value1 = 23;
+
+console.log(value1);
+console.log(++value1);
+console.log(value1);
+console.log(value1++);
+console.log(--value1);
+console.log(value1--);
